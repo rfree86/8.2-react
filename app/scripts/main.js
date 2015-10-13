884e8cf08be0834f207b56f0e10ece500d023f85
Richard <rfree86@gmail.com>
2015-10-13 15:17:18 -0400

    lesson 1 & 2 complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,21 @@
-$(document).ready(function(){
-  // prepend the contents of `app/templates/application.hbs` into `body`
-  $('#container').append(JST.application());
+var App=React.createClass({
+  render(){
+    return(
+
+  <div>
+    <h1>Contacts</h1>
+    <ul>
+      <li>
+        <h2>James Nelson</h2>
+        <a href="mailto:james@jamesknelson.com">james@jamesknelson.com</a>
+      </li>
+      <li>
+        <h2>Joe Citizen</h2>
+        <a href="mailto:joe@example.com">joe@example.com</a>
+      </li>
+    </ul>
+  </div>
+);
+}
 });
+ReactDOM.render(<App/>, document.getElementById('react-app'));
